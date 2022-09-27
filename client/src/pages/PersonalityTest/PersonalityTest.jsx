313738ab6da87b26cdb6fdbb2558dc97294dd8a6
evoulawrence <evoulawrence@gmail.com>
2022-09-27 21:57:48 +0100

    added try and catch block + error message

--- client/src/pages/PersonalityTest/PersonalityTest.jsx
+++ client/src/pages/PersonalityTest/PersonalityTest.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import Option from '../../components/Option/Option'
+import Option from '../../components/Option/Option';
 import Question from '../../components/Question/Question';
 import Button from '../../components/Button/Button';
 import Spinner from '../../components/Spinner/Spinner'; 
@@ -11,31 +11,37 @@ const PersonalityTest = () => {
     const [questions, setQuestions] = useState([]);
     const [currentQuestion, setCurrentQuestion] = useState(1);
     const [personalityType, setPersonalityType] = useState({introvert: 0, extrovert: 0});
-    const [inputValue, setInputValue] = useState('');
+    const [optionValue, setOptionValue] = useState('');
     const [disableButton, setDisableButton] = useState('disable');
     const [selected, setSelected] = useState();
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(false);
 
     const questionsLength = Object.keys(questions).length;
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchQuestions = async () => {
-            const resp = await axios.get(BASE_URL);
-            setQuestions(resp.data);
-            setIsLoading(false);
+            try {
+                const resp = await axios.get(BASE_URL);
+                setQuestions(resp.data);
+                setIsLoading(false);
+            } catch (err) {
+                setError(true);
+            }
+            
         }
         fetchQuestions();
     }, []);
 
     const handleOption = (e, id) => {
-        setInputValue(e.currentTarget.getAttribute('data-personality'));
+        setOptionValue(e.currentTarget.getAttribute('data-personality'));
         setDisableButton('');
         setSelected(id);
     } 
 
     const handleNext = () => {
-        const option = inputValue.toLowerCase();
+        const option = optionValue.toLowerCase();
         personalityType[option] ? personalityType[option] += 1 : personalityType[option] = 1;
         
         const nextQuestion = currentQuestion + 1;
@@ -51,13 +57,14 @@ const PersonalityTest = () => {
 
         setPersonalityType(personalityType);
         setDisableButton('disable');
-        setInputValue('');
+        setOptionValue('');
         setSelected('');
     }
     
     return (
         <div className="quizWrapper">
-            {isLoading && <Spinner />}
+            {isLoading && !error && <Spinner />}
+            {error && <h2 style={{textAlign: 'center', color: '#FF0000'}}>Error while fetching test questions</h2>}
             <>
                 <Question
                     currentQuestion={currentQuestion}
