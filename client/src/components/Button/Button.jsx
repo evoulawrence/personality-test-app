22d19fbed4371d69f9cd351420bfd1d89a9a766c
evoulawrence <evoulawrence@gmail.com>
2022-09-27 03:48:27 +0100

    added resusable button component

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './button.css';
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+const Button = ({ type, clickHandler, buttonDisabled, label }) => {
+    return (
+        <>
+            <button 
+                disabled={buttonDisabled} 
+                className={type} 
+                onClick={() => clickHandler()}
+                >
+                {label}
+            </button>
+        </>
+    )
 }
 
 export default Button
