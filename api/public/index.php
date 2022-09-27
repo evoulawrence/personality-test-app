569968c0fdcaa43e9770181bf8a61e0eaad16ee4
evoulawrence <evoulawrence@gmail.com>
2022-09-27 04:18:39 +0100

    changed api endpoint name

--- api/public/index.php
+++ api/public/index.php
@@ -11,9 +11,9 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
-// all of our endpoints start with /teamway
+// all of our endpoints start with /personality-test
 // everything else results in a 404 Not Found
-if ($uri[1] !== 'teamway') {
+if ($uri[1] !== 'personality-test') {
     header("HTTP/1.1 404 Not Found");
     exit();
 }
